0d029aa65418b2bca39d9c927954f9ece425e0dc
Abhinav <abhisetu610@gmail.com>
2025-03-11 21:47:26 +0530

    Super this

--- src/ArraysAndString/LongestSubstringWithoutRepCharacter.java
+++ src/ArraysAndString/LongestSubstringWithoutRepCharacter.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public class LongestSubstringWithoutRepCharacter {
 
     public static void main(String[] args) {
-      System.out.println(longest("abcdebfbghij"));
+      System.out.println(longest("abcdefe"));
     }
 
     /**
@@ -17,6 +17,7 @@ public class LongestSubstringWithoutRepCharacter {
     private static int longest(String input) {
         Set<Character> hashset= new HashSet<>();
         int left = 0; // Start pointer for the sliding window
+        int maxLength = 0;
         for(int right =0; right< input.length(); right++)
         {
             while(hashset.contains(input.charAt(right))) // to move the start pointer after the repeating character, in out case start will move at 2 index('c')
@@ -25,9 +26,9 @@ public class LongestSubstringWithoutRepCharacter {
                 left++;
             }
             hashset.add(input.charAt(right));
-
+            maxLength = Math.max(maxLength, right - left + 1); //abcdefe is an example to understand
         }
 
-        return hashset.size();
+        return maxLength;
     }
 }
