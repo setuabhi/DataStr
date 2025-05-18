a5013f0bb3f990ada9ef204c537931a5ed0cf9a9
Abhinav <abhisetu610@gmail.com>
2025-05-18 19:08:59 +0530

    Add

--- src/Java8/HighestLenghtString.java
+++ src/Java8/HighestLenghtString.java
@@ -19,12 +19,13 @@ public class HighestLenghtString {
                 new Employee("Jayanjana", "Bengal"),
                 new Employee("Emily", "France")
         );
+        List<String> arrString = new ArrayList<>();
         Optional<Employee> output= arrL.stream().max(Comparator.comparingInt(employee->employee.getName().length()));
         Optional<Employee> outputWithoutComparator= arrL.stream().max((e1,e2)->Integer.compare(e1.getName().length(),e2.getName().length()));
         Optional<Employee> outputReverse= arrL.stream().max((e1,e2)->Integer.compare(e2.getName().length(),e1.getName().length()));
-        //Optional<String> output= arrL.stream().max(Comparator.comparingInt(String::length)); method reference can be used
-        //Optional<String> outputWithoutComparator= arrL.stream().max(s1,s2->Integer.compare(s1.length,s2.length)); method reference can be used
-        //Optional<String> outputReverse= arrL.stream().max(s1,s2->Integer.compare(s2.length,s1.length))); //method reference can be used
+        Optional<String> outputString= arrString.stream().max(Comparator.comparingInt(String::length)); //method reference can be used
+        Optional<String> outputWithoutComparatorString= arrString.stream().max((s1,s2)->Integer.compare(s1.length(),s2.length()));// method reference can be used
+        Optional<String> outputReverseString= arrString.stream().max((s1,s2)->Integer.compare(s2.length(),s1.length())); //method reference can be used
         System.out.println(output.get());
     }
 }
